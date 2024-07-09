46187bc9a8eccb84b620a32e517e96dab61181b8
Tarun Regmi <tarunregmi.io@gmail.com>
2024-07-09 16:46:08 +0545

    use express-status-monitor to monitor express app performance

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import express from 'express';
+import status from 'express-status-monitor';
 
+const PORT = parseInt(process.env.APP_PORT || '8848');
+const HOST = process.env.APP_HOST || 'localhost';
+
+const app = express();
+
+// use express status mointor
+app.use(status());
+
+app.listen(PORT, HOST, () => {
+  console.log(` Server running at http://${HOST}:${PORT}/`);
+});
