28d12c3dccc465a4bd50d72f8d60f3cb200ab7e2
Tarun Regmi <tarunregmi.io@gmail.com>
2024-07-12 16:46:30 +0545

    simple up and running server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import express from 'express';
 
+const PORT = parseInt(process.env.APP_PORT || '8848');
+const HOST = process.env.APP_HOST || 'localhost';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ done: 'done' });
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(` Server running at http://${HOST}:${PORT}/`);
+});
